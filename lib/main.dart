2fd7dafadd3c69e2355a216c84375482b3eae5c9
ersaoktavian <ersaoktavianramadan@gmail.com>
2024-11-05 18:53:40 +0700

    debug false

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Login Register App',
       theme: ThemeData(primarySwatch: Colors.blue),
       initialRoute: '/splash', // Set SplashScreen sebagai route awal
